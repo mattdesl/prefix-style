f050468f13afdbc004e4742d4e97ed21a6f439d6
mattdesl <dave.des@gmail.com>
2014-12-07 12:28:39 -0500

    bam!

---
+++ test.js
@@ -0,0 +1,19 @@
+var prefix = require('./')
+var test = require('tape').test
+
+test('prefixes', function(t) {
+    var valid = [false,  //if browser doesn't support it
+        'transformStyle', 
+        'MozTransformStyle', 
+        'KhtmlTransformStyle', 
+        'WebkitTransformStyle', 
+        'OTransformStyle',
+        'msTransformStyle']
+
+    var result = prefix('transformStyle')
+    t.ok(valid.indexOf(result) !== -1, 'valid: returns '+result)
+
+    t.equal(prefix('width'), 'width', 'does not prefix unless necessary')
+    t.equal(prefix('foobarkashadasha'), false, 'returns false if prop is not supported')
+    t.end()
+})
